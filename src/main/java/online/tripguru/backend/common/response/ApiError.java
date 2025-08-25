3afd391df6bf2ed3c12c1a9c316580004925299b
maxxdroid <theonlymaxmc@gmail.com>
2025-08-25 15:55:54 +0000

    Refactor ApiError class to use Lombok annotations and remove explicit constructor

--- src/main/java/online/tripguru/backend/common/response/ApiError.java
+++ src/main/java/online/tripguru/backend/common/response/ApiError.java
@@ -1,30 +1,25 @@
 package online.tripguru.backend.common.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
 import online.tripguru.backend.common.exception.ApiException;
 
 import java.time.LocalDateTime;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 @Getter
-@Setter
 public class ApiError {
     private int status;
     private String message;
     private LocalDateTime timestamp;
-
-    public ApiError(int status, String message) {
-        this.status = status;
-        this.message = message;
-        this.timestamp = LocalDateTime.now();
-    }
 
     public ApiError(ApiException ex) {
         this.status = ex.getHttpStatus().value();
         this.message = ex.getMessage();
         this.timestamp = LocalDateTime.now();
     }
-
 }
