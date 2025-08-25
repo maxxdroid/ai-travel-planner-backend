e64c70bb09565d90079df1fb5b843dabdd90136d
maxxdroid <theonlymaxmc@gmail.com>
2025-08-25 14:42:31 +0000

    Add abstract ApiException class with HTTP status for standardized error handling

---
+++ src/main/java/online/tripguru/backend/common/exception/ApiException.java
@@ -0,0 +1,14 @@
+package online.tripguru.backend.common.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public abstract class ApiException extends RuntimeException {
+    private final HttpStatus httpStatus;
+
+    protected ApiException(String message, HttpStatus httpStatus) {
+        super(message);
+        this.httpStatus = httpStatus;
+    }
+}
