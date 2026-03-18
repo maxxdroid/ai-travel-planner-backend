6aca0bf7e131b01a22c96b9e36f85a20297d8911
maxxdroid <theonlymaxmc@gmail.com>
2026-03-18 13:15:59 +0000

    Add Lombok annotations to TripGuruUser; update TripController to api/v1/trips, inject GuruService, and return Trip via create

--- src/main/java/online/tripguru/backend/features/trips/controller/TripController.java
+++ src/main/java/online/tripguru/backend/features/trips/controller/TripController.java
@@ -3,6 +3,8 @@ package online.tripguru.backend.features.trips.controller;
 import lombok.RequiredArgsConstructor;
 import online.tripguru.backend.common.response.Response;
 import online.tripguru.backend.features.trips.dto.request.TripsRequest;
+import online.tripguru.backend.features.trips.entity.Trip;
+import online.tripguru.backend.features.trips.service.GuruService;
 import online.tripguru.backend.features.trips.service.TripService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,14 +12,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "/trips")
+@RequestMapping(path = "api/v1/trips")
 @RequiredArgsConstructor
 public class TripController {
-    private final TripService tripService;
+    private final GuruService tripService;
 
     @GetMapping
-    public ResponseEntity<Response<?>> getTripPlans (TripsRequest tripsRequest) {
-        return tripService.getTrips(tripsRequest);
+    public ResponseEntity<Response<Trip>> getTripPlans (TripsRequest tripsRequest) {
+        return tripService.create(tripsRequest);
     }
 
 }
--- src/main/java/online/tripguru/backend/features/trips/entity/TripGuruUser.java
+++ src/main/java/online/tripguru/backend/features/trips/entity/TripGuruUser.java
@@ -1,5 +1,12 @@
 package online.tripguru.backend.features.trips.entity;
 
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
 public class TripGuruUser {
     private String id;
     private String fullName;
