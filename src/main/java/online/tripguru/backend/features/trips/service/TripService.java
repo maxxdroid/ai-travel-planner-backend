273ca3b504ced8aa38eb9a2d6a426036ec2f4246
maxxdroid <theonlymaxmc@gmail.com>
2026-03-18 13:13:57 +0000

    Extend TripService from GuruService; adjust TripServiceImpl (add Trip import, drop implements, comment out @Override)

--- src/main/java/online/tripguru/backend/features/trips/service/TripService.java
+++ src/main/java/online/tripguru/backend/features/trips/service/TripService.java
@@ -4,7 +4,7 @@ import online.tripguru.backend.common.response.Response;
 import online.tripguru.backend.features.trips.dto.request.TripsRequest;
 import org.springframework.http.ResponseEntity;
 
-public interface TripService {
+public interface TripService extends GuruService {
 
     ResponseEntity<Response<?>> getTrips(TripsRequest request);
 }
