dc57b70653e89c24e395db4dde149bca7f6ea420
maxxdroid <theonlymaxmc@gmail.com>
2026-03-24 18:12:21 +0000

    Add Bearer JWT security scheme and requirement to OpenAPI configuration

--- src/main/java/online/tripguru/backend/config/OpenApiConfig.java
+++ src/main/java/online/tripguru/backend/config/OpenApiConfig.java
@@ -2,6 +2,8 @@ package online.tripguru.backend.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,7 +13,7 @@ import java.util.List;
 @Configuration
 public class OpenApiConfig {
 
-//    private static final String SECURITY_SCHEME_NAME = "bearerAuth";
+    private static final String SECURITY_SCHEME_NAME = "bearerAuth";
 
     @Bean
     public OpenAPI customOpenAPI() {
@@ -24,17 +26,17 @@ public class OpenApiConfig {
                                 new Server().url("http://localhost:8080"),
                                 new Server().url("http://api.tripguru.online")
                         )
+                )
+                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes(SECURITY_SCHEME_NAME,
+                                new SecurityScheme()
+                                        .name("Authorization")
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .in(SecurityScheme.In.HEADER)
+                        )
                 );
-//                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
-//                .components(new io.swagger.v3.oas.models.Components()
-//                        .addSecuritySchemes(SECURITY_SCHEME_NAME,
-//                                new SecurityScheme()
-//                                        .name("Authorization")
-//                                        .type(Type.HTTP)
-//                                        .scheme("bearer")
-//                                        .bearerFormat("JWT")
-//                                        .in(In.HEADER)
-//                        )
-//                );
     }
 }
