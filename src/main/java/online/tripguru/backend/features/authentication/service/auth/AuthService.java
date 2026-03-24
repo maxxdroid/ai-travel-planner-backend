4a33d0b6288a3d64509453a9e9db7f6072e4d599
maxxdroid <theonlymaxmc@gmail.com>
2026-03-24 18:11:19 +0000

    Add refresh(Refresh) and logout() methods to AuthService

--- src/main/java/online/tripguru/backend/features/authentication/service/auth/AuthService.java
+++ src/main/java/online/tripguru/backend/features/authentication/service/auth/AuthService.java
@@ -2,6 +2,7 @@ package online.tripguru.backend.features.authentication.service.auth;
 
 import online.tripguru.backend.common.response.Response;
 import online.tripguru.backend.features.authentication.dto.request.LoginRequest;
+import online.tripguru.backend.features.authentication.dto.request.Refresh;
 import online.tripguru.backend.features.authentication.dto.request.SignUpRequest;
 import org.springframework.http.ResponseEntity;
 
@@ -10,5 +11,9 @@ public interface AuthService {
     ResponseEntity<Response<?>> login(LoginRequest request);
 
     ResponseEntity<Response<?>> signup(SignUpRequest signUpRequest);
+
+    public ResponseEntity<Response<?>> refresh(Refresh refresh);
+
+    ResponseEntity<Response<?>> logout();
 
 }
